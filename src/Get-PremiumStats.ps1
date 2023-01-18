0da8c24e0f9aaaf7904c538e020111f4afdeef0e
gratitudist <122744193+gratitudist@users.noreply.github.com>
2023-01-18 15:10:45 -0600

    Supports Fidelity Account_History.csv files

--- src/Get-PremiumStats.ps1
+++ src/Get-PremiumStats.ps1
@@ -7,40 +7,119 @@ Param
     [Switch]$ObjectOut=$False
 )
 
+function Get-FileType {
+  param(
+    [Parameter(Mandatory=$True,Position=0)]
+      [String]$InputFile
+  )
+  $Reader = New-Object System.IO.StreamReader($InputFile)
+  do
+  {
+    $line = $Reader.ReadLine()
+  } while ($line.Length -eq 0)
+  switch -Wildcard ($line) {
+    'Date,Type,Action,Symbol,Instrument*' {
+      'Tastyworks'
+    }
+    'Brokerage' {
+      'Fidelity'
+    }
+  }
+}
+
 function Get-TradeData {
   Param (
     [Parameter(Mandatory=$True,Position=0)]
-      [String]$DataFile
+      [String]$DataFile,
+    [Parameter(Mandatory=$True,Position=1)]
+      [String]$FileType
   )
-  Import-Csv $DataFile | Where-Object { $_.Type -eq 'Trade' }
+  switch ($FileType) {
+    'Tastyworks' {
+      Import-Csv $DataFile | Where-Object { $_.Type -eq 'Trade' }
+    }
+    'Fidelity' {
+      $Reader = New-Object System.IO.StreamReader($DataFile)
+      # Fidelity "CSV" files have four lines of 
+      # crap before the actual csv data begins
+      for($i = 0; $i -lt 5; $i++) {
+        $garbage = $Reader.ReadLine()
+      }
+      Remove-Variable -Name garbage
+      do
+      {
+        $line = $Reader.ReadLine()
+        if ($line.Length) {
+          $data = $data + "`n" + $line
+        }
+      } while ($line.Length)
+      $Header = 'Run Date', 'Account', 'Action', 'Symbol', 'Security Description',
+        'Security Type', 'Exchange Quality', 'Exchange Currency', 'Quantity',
+        'Currency', 'Price', 'Exchange Rate', 'Commission', 'Fees', 'Accrued Interest',
+        'Amount', 'Settlement Date'
+      $data | ConvertFrom-Csv -Header $Header
+    }
+  }
 }
 
 function Get-TradeStats {
   param (
     [Parameter(Mandatory=$True,Position=0)]
-      [PSObject]$TradeData
+      [PSObject]$TradeData,
+    [Parameter(Mandatory=$True,Position=1)]
+      [String]$DataType
   )
   $obj = 0 | Select-Object 'Premium Collected', 'Premium Paid', 'Fees', 'Commissions', 'Profit / Loss', 'Premium Capture Rate'
-  $TradeData | ForEach-Object { $row = $_
-    switch ($row.Action) {
-      'BUY_TO_OPEN' { 
-        $obj.'Premium Paid' += [Math]::Abs($row.Value) 
-        $obj.'Fees' += [Math]::Abs($row.Fees)
-        $obj.'Commissions' += [Math]::Abs($row.Commissions)
+  switch ($DataType) {
+    'Tastyworks' {
+      $TradeData | ForEach-Object { $row = $_
+        switch ($row.Action) {
+          'BUY_TO_OPEN' { 
+            $obj.'Premium Paid' += [Math]::Abs($row.Value) 
+            $obj.'Fees' += [Math]::Abs($row.Fees)
+            $obj.'Commissions' += [Math]::Abs($row.Commissions)
+          }
+          'BUY_TO_CLOSE' { 
+            $obj.'Premium Paid' += [Math]::Abs($row.Value) 
+            $obj.'Fees' += [Math]::Abs($row.Fees)
+          }
+          'SELL_TO_OPEN' { 
+            $obj.'Premium Collected' += [Math]::Abs($row.Value) 
+            $obj.'Fees' += [Math]::Abs($row.Fees)
+            $obj.'Commissions' += [Math]::Abs($row.Commissions)
+          }
+          'SELL_TO_CLOSE' { 
+            $obj.'Premium Collected' += [Math]::Abs($row.Value) 
+            $obj.'Fees' += [Math]::Abs($row.Fees)
+          }
+        }
       }
-      'BUY_TO_CLOSE' { 
-        $obj.'Premium Paid' += [Math]::Abs($row.Value) 
-        $obj.'Fees' += [Math]::Abs($row.Fees)
-      }
-      'SELL_TO_OPEN' { 
-        $obj.'Premium Collected' += [Math]::Abs($row.Value) 
-        $obj.'Fees' += [Math]::Abs($row.Fees)
-        $obj.'Commissions' += [Math]::Abs($row.Commissions)
-      }
-      'SELL_TO_CLOSE' { 
-        $obj.'Premium Collected' += [Math]::Abs($row.Value) 
-        $obj.'Fees' += [Math]::Abs($row.Fees)
-      }
+    }
+    'Fidelity' {
+      $TradeData | ForEach-Object { $row = $_
+        switch -Wildcard ($row.Action) {
+          'YOU BOUGHT OPENING*' {
+            $obj.'Premium Paid' += [Math]::Round([Int]$row.Quantity * 100 * [Decimal]$row.Price, 2)
+            $obj.Fees += [Math]::Round([Decimal]$row.Fees, 2)
+            $obj.Commissions += [Math]::Round([Decimal]$row.Commission, 2)
+          }
+          'YOU BOUGHT CLOSING*' {
+            $obj.'Premium Paid' += [Math]::Round([Int]$row.Quantity * 100 * [Decimal]$row.Price, 2)
+            $obj.Fees += [Math]::Round([Decimal]$row.Fees, 2)
+            $obj.Commissions += [Math]::Round([Decimal]$row.Commission, 2)
+          }
+          'YOU SOLD OPENING*' {
+            $obj.'Premium Collected' += [Math]::Round([Math]::Abs($row.Quantity) * 100 * [Decimal]$row.Price, 2)
+            $obj.Fees += [Math]::Round([Decimal]$row.Fees, 2)
+            $obj.Commissions += [Math]::Round([Decimal]$row.Commission, 2)
+          }
+          'YOU SOLD CLOSING*' {
+            $obj.'Premium Collected' += [Math]::Round([Math]::Abs($row.Quantity) * 100 * [Decimal]$row.Price, 2)
+            $obj.Fees += [Math]::Round([Decimal]$row.Fees, 2)
+            $obj.Commissions += [Math]::Round([Decimal]$row.Commission, 2)
+          }
+        }
+      }    
     }
   }
   $obj.Fees = [Math]::Round($obj.Fees, 2)
@@ -51,12 +130,13 @@ function Get-TradeStats {
 
 if (Test-Path -Path $InputFile)
 {
-  $TradeData = Get-TradeData $InputFile
+  $FileType = Get-FileType -InputFile $InputFile
+  $TradeData = Get-TradeData -DataFile $InputFile -FileType $FileType
 } else {
   Write-Output ("InputFile '{0}' not found." -f $InputFile)
 }
 
-$obj = Get-TradeStats -TradeData $TradeData
+$obj = Get-TradeStats -TradeData $TradeData -DataType $FileType
 
 if ($ObjectOut) {
   $obj
